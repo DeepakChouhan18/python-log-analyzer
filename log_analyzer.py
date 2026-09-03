cbf3ac76402dffbdc8158f44bcdb18d9bc50e763
Deepak Chouhan <22deepakchouhank@gmail.com>
2026-09-04 00:45:27 +0530

    Add log level counting to log_analyzer.py
    Count occurrences of INFO, WARNING, and ERROR in log.

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,3 +1,16 @@
+info_count = 0
+warning_count = 0
+error_count = 0
+
 with open("sample.log", "r") as file:
     for line in file:
-        print(line.strip())
+        if "INFO" in line:
+            info_count += 1
+        elif "WARNING" in line:
+            warning_count += 1
+        elif "ERROR" in line:
+            error_count += 1
+
+print("INFO:", info_count)
+print("WARNING:", warning_count)
+print("ERROR:", error_count)
