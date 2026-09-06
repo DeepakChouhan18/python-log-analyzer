74a59f7884ccafe24d5573c2e88827ae76f96925
Deepak Chouhan <22deepakchouhank@gmail.com>
2026-09-06 14:25:06 +0530

    Refactor log counting to use a dictionary

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,16 +1,18 @@
-info_count = 0
-warning_count = 0
-error_count = 0
+log_counts = {
+    "INFO": 0,
+    "WARNING": 0,
+    "ERROR": 0
+}
 
 with open("sample.log", "r") as file:
     for line in file:
         if "INFO" in line:
-            info_count += 1
+            log_counts["INFO"] += 1
         elif "WARNING" in line:
-            warning_count += 1
+            log_counts["WARNING"] += 1
         elif "ERROR" in line:
-            error_count += 1
+            log_counts["ERROR"] += 1
 
-print("INFO:", info_count)
-print("WARNING:", warning_count)
-print("ERROR:", error_count)
+print("INFO:", log_counts["INFO"])
+print("WARNING:", log_counts["WARNING"])
+print("ERROR:", log_counts["ERROR"])
